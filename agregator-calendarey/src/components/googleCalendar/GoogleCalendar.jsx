1cf8eb6b042f7d1de5e48f89736e4dce60547aad
Alexeycreator <rushev.alexey@gmail.com>
2021-04-20 21:16:09 +0300

    Подготовка компонент

---
+++ agregator-calendarey/src/components/googleCalendar/GoogleCalendar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./GoogleCalendar.module.css";
+
+const GoogleCalendar = (props) => {
+  return (
+    <div className={s.main}>
+      Календарь
+    </div>
+  );
+};
+
+export default GoogleCalendar;
---
+++ agregator-calendarey/src/components/menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Menu.module.css';
+
+const Menu = (props) => {
+    return(
+        <div className={s.menu}>
+            Меню
+        </div>
+    );
+}
+
+export default Menu;
